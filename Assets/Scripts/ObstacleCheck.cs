483ae8a9f94133dbf7f4cad3541bbf8e69cf8a75
jpprodriguez <juampi.peveri@hotmail.com>
2016-06-05 23:12:51 -0300

    cambio textura de pinche por cactus y agrego textura de perro #7

--- Assets/Scripts/ObstacleCheck.cs
+++ Assets/Scripts/ObstacleCheck.cs
@@ -4,11 +4,7 @@ using System.Collections;
 public class ObstacleCheck : MonoBehaviour {
 
     private Player player;
-    public float time = 0.02f;
-    public float pwrX = 100;
-    public float pwrY = 350;
-    //public float posX;
-    //public float posY;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
@@ -22,12 +18,5 @@ public class ObstacleCheck : MonoBehaviour {
         }
         
     }
-    /*
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        player.Damage(1);
-        player.onTouchObstacle(posX, posY);
-    }
-    */
 
 }
