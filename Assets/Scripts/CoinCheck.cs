259350a9e07ed655c8755e166401858f3a4080cd
jppeveri <jppeveri@debmedia.com>
2016-06-07 00:58:38 -0300

    Agrego monedas #10

---
+++ Assets/Scripts/CoinCheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCheck : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            GameData.setScore(GameData.getScore() + 50);
+            Destroy(gameObject);
+        }
+
+    }
+}
