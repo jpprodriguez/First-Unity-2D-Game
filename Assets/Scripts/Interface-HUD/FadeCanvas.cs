1b63482f9d24306012ba2aa1f624122c562caa54
jpprodriguez <juampi.peveri@hotmail.com>
2016-06-11 19:03:51 -0300

    no se hago cosas me da paja comitear #15

---
+++ Assets/Scripts/Interface-HUD/FadeCanvas.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeCanvas : MonoBehaviour {
+
+    public CanvasGroup canvas;
+	// Use this for initialization
+    public void fadeCanvas()
+    {
+        StartCoroutine(doFade()) ;
+    }
+    private IEnumerator doFade()
+    {
+        while(canvas.alpha > 0)
+        {
+            canvas.alpha -= Time.deltaTime / 2;
+            yield return 0;
+        }
+    }
+}
