6b1947603a5cc7ae8b8d335646c20edd98e4586a
jpprodriguez <juampi.peveri@hotmail.com>
2016-06-13 23:28:01 -0300

    comiteo todo, me pudri #17

---
+++ Assets/Scripts/Enemies-Obstacles/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float maxDistanceX;
+    public float maxDistanceY;
+
+    private Vector2 originalPos;
+	// Use this for initialization
+	void Start () {
+        originalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(transform.position.x - originalPos.x > maxDistanceX || transform.position.y - originalPos.y > maxDistanceY)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
